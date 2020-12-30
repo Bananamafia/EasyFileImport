1098c356cee4bade64bea83690ce7e328b9a3927
Max <63409017+Bananamafia@users.noreply.github.com>
2020-12-30 20:59:22 +0100

    creates directory on desktop, which name is based on the current date

--- FileImportConsoleApp/Program.cs
+++ FileImportConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileImportConsoleApp
 {
@@ -6,7 +7,16 @@ namespace FileImportConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Directory.CreateDirectory(fullDirectoryPath);
+            Console.WriteLine("Ordner wurde erstellt.");
+        }
+
+
+        private static string directoryPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}";
+        private static string directoryName = $"{DateTime.Today.ToString("yyyy-MM-dd")}_import";
+        private static string fullDirectoryPath
+        {
+            get { return $@"{directoryPath}\{directoryName}"; }
         }
     }
 }
