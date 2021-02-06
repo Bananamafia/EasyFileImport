4e21652252d60b03b42f3f18fdfb1d3f9fd4bc07
Max <63409017+Bananamafia@users.noreply.github.com>
2021-02-06 12:18:43 +0100

    Added Properties for inserted device and number of selected files

--- FileImportDesktopApp/ViewModels/MainViewModel.cs
+++ FileImportDesktopApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FileImportDesktopApp.ViewModels
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+        private string _deviceName;
+        public string DeviceName
+        {
+            get
+            { return _deviceName; }
+            set
+            {
+                _deviceName = value;
+                OnPropertyChanged("DeviceName");
+                OnPropertyChanged("SelectedFilesCount");
+            }
+        }
+
+        public int SelectedFilesCount { get; set; }
+
+        //---INotifyPropertyChanged---
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
     }
 }
