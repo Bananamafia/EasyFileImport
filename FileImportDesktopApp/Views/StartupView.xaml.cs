69361bf700c789e0d8c8489829f18fff2d4d70a5
Bananamafia <maximilian.kiefer@yahoo.de>
2021-02-06 17:57:40 +0100

    Created Rough Startup View

---
+++ FileImportDesktopApp/Views/StartupView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using FileImportDesktopApp.ViewModels;
+
+namespace FileImportDesktopApp.Views
+{
+    /// <summary>
+    /// Interaktionslogik für StartupView.xaml
+    /// </summary>
+    public partial class StartupView : Window
+    {
+        private StartupViewModel _viewModel;
+
+        public StartupView()
+        {
+            InitializeComponent();
+            _viewModel = new StartupViewModel();
+            this.DataContext = _viewModel;
+        }
+    }
+}
