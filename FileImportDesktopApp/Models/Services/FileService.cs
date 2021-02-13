eb3d02a9a95248ba83f5c0656f10ef79f09fa181
Bananamafia <maximilian.kiefer@yahoo.de>
2021-02-13 18:29:48 +0100

    Added number of files property to FileService

--- FileImportDesktopApp/Models/Services/FileService.cs
+++ FileImportDesktopApp/Models/Services/FileService.cs
@@ -9,6 +9,26 @@ namespace FileImportDesktopApp.Models.Services
 {
     public class FileService
     {
+        public FileService(ImportConfigSettings configSettings)
+        {
+            _configSettings = configSettings;
+        }
 
+        private ImportConfigSettings _configSettings;
+
+        public List<string> SelectedFilePaths()
+        {
+            List<string> files = Directory.GetFiles(_configSettings.DevicePath, "*.*", _configSettings.DirectoryDepth).ToList();
+
+            return files;
+        }
+
+        public int NumberOfFiles
+        {
+            get
+            {
+                return SelectedFilePaths().Count();
+            }
+        }
     }
 }
