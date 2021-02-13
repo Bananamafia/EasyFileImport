95ae913ad213c845cc4829e8ee1dcd1e96688c66
Bananamafia <maximilian.kiefer@yahoo.de>
2021-02-13 18:30:41 +0100

    Added class for config settings

---
+++ FileImportDesktopApp/Models/ImportConfigSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileImportDesktopApp.Models
+{
+    public class ImportConfigSettings
+    {
+        public ImportConfigSettings(string devicePath)
+        {
+            _devicePath = devicePath;
+        }
+
+        private string _devicePath;
+
+        public string DevicePath
+        {
+            get { return _devicePath; }
+        }
+
+        public TimePeriod timePeriod { get; set; } = TimePeriod.allTime;
+
+        public DateTime BeginningDate { get; set; } = DateTime.Today;
+        public DateTime EndingDate { get; set; } = DateTime.Today;
+        public SearchOption DirectoryDepth { get; set; } = SearchOption.AllDirectories;
+    }
+}
