51a737c929ec3e4b099464daa8814c58eec9a338
Bananamafia <maximilian.kiefer@yahoo.de>
2021-02-15 17:58:22 +0100

    Added importDirectory Service

---
+++ FileImportDesktopApp/Models/Services/ImportDirectoryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileImportDesktopApp.Models.Services
+{
+    class ImportDirectoryService
+    {
+        private static string directoryPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}";
+
+        private static string directoryName = $"{DateTime.Today.ToString("yyyy-MM-dd")}_import";
+
+        public static string fullDirectoryPath
+        {
+            get { return $@"{directoryPath}\{directoryName}"; }
+        }
+
+        public static void CreateImportDirectory()
+        {
+            Directory.CreateDirectory(fullDirectoryPath);
+        }
+    }
+}
